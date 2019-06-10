5585e2563b95256383b3b37bfa863ee4ef5a163e
hmazomba <hlumzo@gmail.com>
2019-06-10 17:32:38 +0200

    Added a music player context component

--- moose/src/App.js
+++ moose/src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TrackList from '../src/components/TrackList';
+
+import { MusicPlayerProvider } from './components/MusicPlayerContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MusicPlayerProvider>
+      <div className="container">
+        <TrackList/>
+      </div>
+    </MusicPlayerProvider>
   );
 }
 
